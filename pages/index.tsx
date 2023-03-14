842bd63804ee2f0af89aba72ea981ca5a2eb2153
Rodrigo Fentanes <rfentanes@sga.pucminas.br>
2023-03-14 17:20:51 -0300

    unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import Intro from "../components/Intro";
 import AboutMe from "../components/AboutMe";
 import Header from "../components/Header";
